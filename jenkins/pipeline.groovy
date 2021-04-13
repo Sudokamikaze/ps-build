7bca68199cfe87a18c9acaa83fc44c0103d0fb24
Illia P <sudokamikaze@protonmail.com>
2021-04-13 16:25:23 +0300

    tests

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -233,7 +233,7 @@ pipeline {
             steps {
                 timeout(time: pipeline_timeout, unit: 'HOURS')  {
                     retry(3) {
-                        git branch: '8.0', url: 'https://github.com/Percona-Lab/ps-build'
+                        git branch: 'PS-7639-testingground', url: 'https://github.com/Sudokamikaze/ps-build'
                         withCredentials([string(credentialsId: 'MTR_VAULT_TOKEN', variable: 'MTR_VAULT_TOKEN')]) {
                             withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'c8b933cd-b8ca-41d5-b639-33fe763d3f68', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                                 sh '''
